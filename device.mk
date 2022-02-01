e020c8d5f23e9bd59b444e177aa610852cfd98c4
dianlujitao <dianlujitao@lineageos.org>
2022-02-01 16:02:42 +0800

    nio: Add touch HAL to handle single tap gesture
    Change-Id: I3a29cffe4b90581a1bc7b412cbb1396aa0904b88

--- device.mk
+++ device.mk
@@ -443,6 +443,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.nio
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service.kona

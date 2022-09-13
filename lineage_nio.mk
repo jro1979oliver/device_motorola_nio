61c6b2f41b613634685b4a53ebb4fc07c85f9c00
dianlujitao <dianlujitao@lineageos.org>
2022-09-13 18:59:29 +0800

    nio: Fix overriding ro.product.*.name
    Change-Id: I7326654087050683d334c39160ae435012b89d77

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -39,11 +39,13 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 2520
 TARGET_SCREEN_WIDTH := 1080
 
+PRODUCT_SYSTEM_NAME := nio_retcn
+
 # Build info
 BUILD_FINGERPRINT := "motorola/nio_retcn/nio:12/S1RN32.55-16-2/125d6-32b4dd:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=nio_retcn \
-    PRIVATE_BUILD_DESC="nio_retcn-user 12 S1RN32.55-16-2 125d6-32b4dd release-keys"
+    PRIVATE_BUILD_DESC="nio_retcn-user 12 S1RN32.55-16-2 125d6-32b4dd release-keys" \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 

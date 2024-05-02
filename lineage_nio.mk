e53e73008ffc0f5b8e6d4ec54a49f1314dff3787
jro1979oliver <jroliveira.oliveira301@gmail.com>
2024-05-02 20:43:39 +0200

    nio: Bringup RisinOS
    This reverts commit 4c2ac859ad8326f3f684d96bf802cfd3ff3440e3.

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from nio device
 $(call inherit-product, device/motorola/nio/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_nio
+PRODUCT_NAME := lineage_nio
 PRODUCT_DEVICE := nio
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(100)
@@ -28,3 +28,12 @@ BUILD_FINGERPRINT := "motorola/nio_retail/nio:12/S1RTS32.41-20-16-1-3/22842-f0a4
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="nio_retail-user 12 S1RTS32.41-20-16-1-3 22842-f0a4cf release-keys" \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+# Rising flags
+RISING_MAINTAINER := jro1979oliver
+WITH_GMS := true
+TARGET_PREBUILT_GOOGLE_CAMERA := false
+TARGET_ENABLE_BLUR := true
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_MAINTAINER="jro1979oliver"
+    RISING_CHIPSET="kona"
